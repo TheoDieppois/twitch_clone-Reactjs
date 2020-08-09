d0be15340418e8f0d4152712d98bc5094dfa9066
dieppthe <theo76.dieppois@gmail.com>
2020-08-09 23:07:56 +0200

    api key

--- src/api.js
+++ src/api.js
@@ -1,19 +1,10 @@
 import axios from 'axios'
-import { clientID, token } from './private'
 
 const api = axios.create({
     headers : {
-        'Client-ID' : clientID,
-        'Authorization' : `Bearer ${token}`
+        'Client-ID' : '2rlwgambyipcraogkk832tuirojeko',
+        'Authorization' : 'Bearer 2tfxzo7rxsdz6b6tpqockxhfb3dye4'
     }
 })
-
-/*
-    CLIENT_ID = '2rlwgambyipcraogkk832tuirojeko';
-    REDIRECT = 'http://127.0.0.1/';
-
-    LIEN AUTH = https://id.twitch.tv/oauth2/authorize?client_id=2rlwgambyipcraogkk832tuirojeko&redirect_uri=http://localhost:3000/&response_type=token
-
-*/
 
 export default api
